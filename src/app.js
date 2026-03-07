4c76f6bd07e542c9de719dadac84ae2c7d88bad3
sah-lishi <lishisaah@gmail.com>
2026-03-07 11:13:06 +0530

    feat: implement rate limit

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 import express from "express"
 import router from "./routes/travel.routes.js"
+import rateLimit from "express-rate-limit"
 
 const app = express()
 
 app.use(express.json())
+
+const rateLimit = rateLimit({
+    window: 20 * 60 * 1000,
+    max: 50,
+    message: "Too many requests..."
+})
+app.use(rateLimit)
 app.use("/", router)
 export default app
