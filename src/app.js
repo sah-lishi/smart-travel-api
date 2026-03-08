88fcec4b4ff7fb58c8c9b6c8637ae1f229464a91
sah-lishi <lishisaah@gmail.com>
2026-03-08 15:09:28 +0530

    feat: add global error handling middleware

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express"
 import travelRoute from "./routes/travel.routes.js"
 import metricRouter from "./routes/metrics.route.js"
 import rateLimit from "express-rate-limit"
+import _config from "./config/index.js"
 
 const app = express()
 
@@ -12,7 +13,23 @@ app.use(rateLimit({
     max: 50,
     message: "Too many requests..."
 }))
+
 app.use("/api/v1/travel", travelRoute)
 app.use("/api/v1/metrics", metricRouter)
+
+app.use((err, req, res, next) => {
+    if(err.message === "city not found") {
+        return res.status(404).json({
+            success: false,
+            message: err.message
+        })
+    }
+
+    return res.status(err.statusCode || 500).json({
+        success: false,
+        message: err.message || "Internal server error",
+        stack: _config.node_env === "production" ? undefined : err.stack
+    })
+})
 
 export default app
